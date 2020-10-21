29a099fff4820e2adb4169e4d8e14e96ee18f8bb
Lida Wang <70541830+LidaWangPSU@users.noreply.github.com>
2020-10-20 21:30:08 -0400

    Update HW6_SVM.R

--- HW6_SVM.R
+++ HW6_SVM.R
@@ -6,6 +6,14 @@ a<-matrix(c(0.5,0.1,
             0,0.5),2,2)
 Sigma<-a%*%t(a)
 
+set.seed(3)
+mu1<-c(2,2)
+mu2<-c(0,0)
+a1<-mvrnorm(100,mu1,Sigma)
+a2<-mvrnorm(100,mu2,Sigma)
+data0<-rbind(cbind(rep(1,100),a1),cbind(rep(-1,100),a2))
+colnames(data0)<-c("group","x1","x2")
+
 set.seed(6)
 mu1<-c(2,2)
 mu2<-c(0,0)
@@ -13,8 +21,6 @@ a1<-mvrnorm(100,mu1,Sigma)
 a2<-mvrnorm(100,mu2,Sigma)
 data1<-rbind(cbind(rep(1,100),a1),cbind(rep(-1,100),a2))
 colnames(data1)<-c("group","x1","x2")
-plot(data1[1:100,2:3],xlim=c(-2,4),ylim=c(-2,4),col="red",xlab = "x1",ylab="x2")
-points(data1[101:200,2:3],col="blue")
 
 
 svm_0<-function(data){
@@ -111,6 +117,28 @@ if(min(x1%*%beta)+beta0<1){
 }
   
 }
+
+###two supports case
+plot(data0[1:100,2:3],xlim=c(-2,4),ylim=c(-2,4),col="red",xlab = "x1",ylab="x2")
+points(data0[101:200,2:3],col="blue")
+
+svm_0(data0)
+###compare with svm
+m0<-svm(group~x1+x2,data=data0,kernel="linear",scale=FALSE,type="C-classification")
+coef(m1)
+cf <- coef(m0)
+abline(-cf[1]/cf[3], -cf[2]/cf[3], col = "red")
+#plot margin 
+abline(-(cf[1] + 1)/cf[3], -cf[2]/cf[3], col = "blue",lty=2)
+abline(-(cf[1] - 1)/cf[3], -cf[2]/cf[3], col = "blue",lty=2)
+legend("bottomleft", legend=c("boundary by SVM package", "margin by SVM package","boundary by myself","margin by myself"),
+       col=c("red", "blue","green","grey"), lty=c(1,2,1,2), cex=0.8)
+title(main="SVM")
+
+
+###three supports case
+plot(data1[1:100,2:3],xlim=c(-2,4),ylim=c(-2,4),col="red",xlab = "x1",ylab="x2")
+points(data1[101:200,2:3],col="blue")
 
 svm_0(data1)
 ###compare with svm
